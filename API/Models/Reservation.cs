60e4c72a060bd16c747dd1b9ea8e1f6848a4b258
ze-fernando <zefernando.dev@gmail.com>
2024-12-27 16:24:50 -0300

    feat: remove required id

--- API/Models/Reservation.cs
+++ API/Models/Reservation.cs
@@ -4,11 +4,11 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Models
 {
-    class Reservation
+    public class Reservation
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public required string Id { get; set; }
+        public string Id { get; set; }
         public required int NumberPeople { get; set; }
         public required DateTime Date { get; set; }
         public required string ReservationName { get; set; }
